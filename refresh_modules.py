ansible-collections/vmware_rest_code_generator

commit 71ee926df6f11f0d9b0dabef802124cb071c04c3

ability to use the "label" to identify resoure
Add the ability to use the resource "label" to identify a resource.

refresh_modules.py
@@ -358,6 +358,17 @@ class AnsibleModuleBase:
                 payload[operationId][_in][parameter["name"]] = payload_info
         return payload
 
+    def answer(self):
+        try:
+            raw_answer = flatten_ref(
+                self.resource.operations["get"][3]["200"], self.definitions
+            )
+            fields = raw_answer["schema"]["properties"]["value"]["properties"]
+        except KeyError:
+            return
+
+        return fields
+
     def parameters(self):
         def sort_operationsid(input):
             output = sorted(input)
@@ -417,6 +428,12 @@ class AnsibleModuleBase:
 
                 results[name]["operationIds"].append(operationId)
                 results[name]["operationIds"].sort()
+
+        answer_fields = self.answer()
+        # Note: If the final result comes with a "label" field, we expose a "label"
+        # parameter. We will use the field to identify an existing resource.
+        if answer_fields and "label" in answer_fields:
+            results["label"] = {"type": "str", "name": "label"}
 
         for name, result in results.items():
             if result.get("enum"):
@@ -567,6 +584,7 @@ try:
 except ImportError:
     from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.vmware.vmware_rest.plugins.module_utils.vmware_rest import (
+    build_full_device_list,
     exists,
     gen_args,
     get_device_info,
@@ -684,7 +702,7 @@ async def entry_point(module, session):
         main_func = ast.parse(MAIN_FUNC.format(name=self.name))
 
         for operation in sorted(self.default_operationIds):
-            (verb, path, _) = self.resource.operations[operation]
+            (verb, path, _, _) = self.resource.operations[operation]
             if not path.startswith("/rest"):  # TODO
                 path = "/rest" + path
             if "$" in operation:
@@ -777,7 +795,7 @@ async def _update(params, session):
             FUNC_WITH_DATA_CREATE_TPL = """
 async def _create(params, session):
     if params["{list_index}"]:
-        _json = await get_device_info(params, session, build_url(params), params["{list_index}"])
+        _json = await get_device_info(session, build_url(params), params["{list_index}"])
     else:
         _json = await exists(params, session, build_url(params), ["{list_index}"])
     if _json:
@@ -803,7 +821,7 @@ async def _create(params, session):
                 _id = list(_json["value"].values())[0]
             else:
                 _id = _json["value"]
-            _json = await get_device_info(params, session, _url, _id)
+            _json = await get_device_info(session, _url, _id)
 
         return await update_changed_flag(_json, resp.status, "{operation}")
 """
@@ -892,10 +910,22 @@ return (
     def gen_entry_point_func(self):
         FUNC = """
 async def entry_point(module, session):
-    async with session.get(build_url(module.params)) as resp:
+    url = build_url(module.params)
+    async with session.get(url) as resp:
         _json = await resp.json()
         if module.params.get("{list_index}"):
             _json["id"] = module.params.get("{list_index}")
+        elif module.params.get("label"):  # TODO extend the list of filter
+            _json = await exists(module.params, session, url)
+        else: # list context, retrieve the details of each entry
+            try:
+                if isinstance(_json["value"][0]["{list_index}"], str) and len(list(_json["value"][0].values())) == 1:
+                    # this is a list of id, we fetch the details
+                    full_device_list = await build_full_device_list(session, url, _json)
+                    _json = {{"value": [i["value"] for i in full_device_list]}}
+            except (KeyError, IndexError):
+                pass
+
         return await update_changed_flag(_json, resp.status, "get")
 """
         template = FUNC if self.list_index() else FUNC
@@ -971,6 +1001,7 @@ class SwaggerFile:
                     verb,
                     path.path,
                     desc["parameters"],
+                    desc["responses"],
                 )
         return result
 

module_utils/vmware_rest.py
@@ -132,23 +132,34 @@ async def update_changed_flag(data, status, operation):
     return data
 
 
-async def list_devices(params, session, url):
+async def list_devices(session, url):
     existing_entries = []
+
     async with session.get(url) as resp:
         _json = await resp.json()
-        devices = _json["value"]
-    device_type = get_device_type(url)
-    subdevice_type = get_subdevice_type(url)
-
-    for device in devices:
-        _id = device.get(device_type) or device.get(subdevice_type)
-        if not _id:
-            raise Exception("Cannot find the id key of the device!")
-        existing_entries.append((await get_device_info(params, session, url, _id)))
-    return existing_entries
+        return _json
 
 
-async def get_device_info(params, session, url, _id):
+async def build_full_device_list(session, url, device_list):
+    import asyncio
+
+    device_ids = []
+    for i in device_list["value"]:
+        fields = list(i.values())
+        key = list(i.keys())[0]
+        if len(fields) != 1:
+            # The list already comes with all the details
+            return device_list
+        device_ids.append(fields[0])
+
+    tasks = [
+        asyncio.ensure_future(get_device_info(session, url, _id)) for _id in device_ids
+    ]
+
+    return [await i for i in tasks]
+
+
+async def get_device_info(session, url, _id):
     async with session.get(url + "/" + _id) as resp:
         _json = await resp.json()
         _json["id"] = str(_id)
@@ -159,11 +170,12 @@ async def exists(params, session, url, unicity_keys=None):
     if not unicity_keys:
         unicity_keys = []
 
-    unicity_keys += ["pci_slot_number", "sata"]
+    unicity_keys += ["label", "pci_slot_number", "sata"]
 
-    devices = await list_devices(params, session, url)
+    devices = await list_devices(session, url)
+    full_devices = await build_full_device_list(session, url, devices)
 
-    for device in devices:
+    for device in full_devices:
         for k in unicity_keys:
             if not params.get(k):
                 continue
@@ -217,5 +229,7 @@ def get_device_type(url):
         return "nic"
     elif device_type in ["sata", "scsi"]:
         return "adapter"
+    elif device_type in ["parallel", "serial"]:
+        return "port"
     else:
         return device_type

commit 829e67bf3199f2dbe59c4a35e344329de9260143

fix when list_index is None
Fix a regression introduced in 71ee926df6f11f0d9b0dabef802124cb071c04c3

refresh_modules.py
@@ -923,7 +923,7 @@ async def entry_point(module, session):
                     # this is a list of id, we fetch the details
                     full_device_list = await build_full_device_list(session, url, _json)
                     _json = {{"value": [i["value"] for i in full_device_list]}}
-            except (KeyError, IndexError):
+            except (TypeError, KeyError, IndexError):
                 pass
 
         return await update_changed_flag(_json, resp.status, "get")