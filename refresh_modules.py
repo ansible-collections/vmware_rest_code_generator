a6d2a23f6bef8412e7badc529affb6e56f602ef2
Gonéri Le Bouder <goneri@lebouder.net>
2020-09-30 17:23:03 -0400

    ensure the vcenter_* parameters are set

--- refresh_modules.py
+++ refresh_modules.py
@@ -776,6 +776,12 @@ def prepare_argument_spec():
 async def main( ):
     module_args = prepare_argument_spec()
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
+    if not module.params['vcenter_hostname']:
+        module.fail_json('vcenter_hostname cannot be empty')
+    if not module.params['vcenter_username']:
+        module.fail_json('vcenter_username cannot be empty')
+    if not module.params['vcenter_password']:
+        module.fail_json('vcenter_password cannot be empty')
     session = await open_session(vcenter_hostname=module.params['vcenter_hostname'], vcenter_username=module.params['vcenter_username'], vcenter_password=module.params['vcenter_password'], log_file=module.params['vcenter_rest_log_file'])
     result = await entry_point(module, session)
     module.exit_json(**result)
