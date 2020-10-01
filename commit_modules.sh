2443b4804b36cee0cf3ec93d8b8a37dd2d34fbf6
Gonéri Le Bouder <goneri@lebouder.net>
2020-10-01 09:22:33 -0400

    import commit_modules.sh

---
+++ commit_modules.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Use this script to commit an update of the vmare_rest collection
+tox -e refresh_modules -- --target-dir ~/.ansible/collections/ansible_collections/vmware/vmware_rest
+tox -e refresh_examples -- --target-dir ~/.ansible/collections/ansible_collections/vmware/vmware_rest
+source ~/tmp/ansible_2.11/bin/activate
+cd ~/.ansible/collections/ansible_collections/vmware/vmware_rest
+set -eux
+mkdir -p logs
+(
+    cd ~/.ansible/collections/ansible_collections/vmware/vmware_rest/tests/integration/targets/vcenter_vm_scenario1
+    ./refresh_RETURN_block.sh
+)
+tox -e black
+ansible-test sanity --debug --requirements --local --skip-test future-import-boilerplate --skip-test metaclass-boilerplate --python 3.8 -vvv
+tox -e add_docs
+git add README.md dev.md plugins docs tests/sanity/ignore-*.txt
+git add changelogs
+git commit -S -F commit_message
