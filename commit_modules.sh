d5a85ccf2bd4c875873250172f0a760512b552b1
Gonéri Le Bouder <goneri@lebouder.net>
2020-10-08 14:22:03 -0400

    commit_modules: generate the HTML doc
    Depends-On: https://github.com/ansible-collections/vmware_rest_code_generator/pull/49

--- commit_modules.sh
+++ commit_modules.sh
@@ -15,10 +15,24 @@ mkdir -p logs
     cd ~/.ansible/collections/ansible_collections/goneri/utils
     ./scripts/inject_RETURN.py ~/.ansible/collections/ansible_collections/vmware/vmware_rest/manual/source/vmware_rest_scenarios/task_outputs/ ~/git_repos/ansible-collections/vmware_rest/ --config-file config/inject_RETURN.yaml
 )
-tox -e build_manual
 tox -e black
-ansible-test sanity --debug --requirements --local --skip-test future-import-boilerplate --skip-test metaclass-boilerplate --python 3.8 -vvv
 tox -e add_docs
-git add README.md dev.md plugins docs tests/sanity/ignore-*.txt
+
+(
+    cd manual/source
+    echo "****************************************
+Manual of the vmware.vmware_rest modules
+****************************************
+
+.. toctree::
+    :maxdepth: 1
+" > docs.rst
+    ln -s ../../docs .
+    find docs/ -name '*.rst'|grep '/'|sort|sed 's,\(.*\).rst$,    \1,' >> docs.rst
+    tox -e build_manual
+    rm docs
+)
+ansible-test sanity --debug --requirements --local --skip-test future-import-boilerplate --skip-test metaclass-boilerplate --python 3.8 -vvv
+git add README.md dev.md plugins docs manual tests/sanity/ignore-*.txt
 git add changelogs
 git commit -S -F commit_message
