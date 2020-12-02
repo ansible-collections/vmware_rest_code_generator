64942335eefce71008288cefed901c4d8e77ba3f
Gonéri Le Bouder <goneri@lebouder.net>
2020-12-02 13:32:52 -0500

    tox: enforce black and flake8 on module_utils dir
    Ensure we also validate the format of the the module_utils directory.
    
    Signed-off-by: Gonéri Le Bouder <goneri@lebouder.net>

--- module_utils/test_vmware_rest.py
+++ module_utils/test_vmware_rest.py
@@ -3,5 +3,5 @@ from vmware_rest import get_subdevice_type
 
 def test_get_subdevice_type():
     assert get_subdevice_type("http://a/{b}/b/{c}/d") == "c"
-    assert get_subdevice_type("http://a/{b}/b/{c}/d/{e}") == None
-    assert get_subdevice_type("http://a/{b}/b") == None
+    assert get_subdevice_type("http://a/{b}/b/{c}/d/{e}") is None
+    assert get_subdevice_type("http://a/{b}/b") is None
