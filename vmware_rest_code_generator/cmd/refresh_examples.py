8e1d5489951454b778d13f093773fbed4819799d
Gonéri Le Bouder <goneri@lebouder.net>
2021-05-03 17:30:45 -0400

    example: better dep handling
    - follow the natural execution flow
    - allow more than one single dependency level

--- vmware_rest_code_generator/cmd/refresh_examples.py
+++ vmware_rest_code_generator/cmd/refresh_examples.py
@@ -16,22 +16,64 @@ def _task_to_string(task):
     return a.read().rstrip()
 
 
-def get_tasks(target_dir, *scenarios):
+def get_tasks(target_dir, play="main.yaml"):
     tasks = []
-    for scenario in scenarios:
-        task_dir = target_dir / "tests" / "integration" / "targets" / scenario / "tasks"
-        for _file in task_dir.glob("*"):
-            _yaml = ruamel.yaml.YAML()
-            _yaml.indent(sequence=4, offset=2)
-            tasks += _yaml.load(_file.open())
+    current_file = target_dir / play
+    _yaml = ruamel.yaml.YAML()
+    _yaml.indent(sequence=4, offset=2)
+    for task in _yaml.load(current_file.open()):
+        if "include_tasks" in task:
+            tasks += get_tasks(target_dir, play=task["include_tasks"])
+        elif "import_tasks" in task:
+            tasks += get_tasks(target_dir, play=task["import_tasks"])
+        else:
+            tasks.append(task)
     return tasks
+
+
+def naive_variable_from_jinja2(raw):
+    jinja2_string = raw.strip(" }{}")
+    if "lookup(" in jinja2_string:
+        return None
+    if jinja2_string.startswith("not "):
+        jinja2_string = jinja2_string[4:]
+    variable = jinja2_string.split(".")[0]
+    if variable == "item":
+        return None
+    return variable
+
+
+def list_dependencies(task):
+    dependencies = []
+    if isinstance(task, str):
+        if task[0] != "{":
+            return []
+        variable = naive_variable_from_jinja2(task)
+        if variable:
+            return [variable]
+    for k, v in task.items():
+        if isinstance(v, dict):
+            dependencies += list_dependencies(v)
+        elif isinstance(v, list):
+            for i in v:
+                dependencies += list_dependencies(i)
+        elif not isinstance(v, str):
+            pass
+        elif "{{" in v:
+            variable = naive_variable_from_jinja2(v)
+            if variable:
+                dependencies.append(variable)
+        elif k == "with_items":
+            dependencies.append(v.split(".")[0])
+    dependencies = [i for i in dependencies if not i.startswith("_")]
+    return list(set(dependencies))
 
 
 def extract(tasks, collection_name):
     by_modules = collections.defaultdict(dict)
     registers = {}
 
-    for task in sorted(tasks, key=lambda item: (item.get("name", "")),):
+    for task in tasks:
         if "name" not in task:
             continue
 
@@ -39,24 +81,25 @@ def extract(tasks, collection_name):
             print(f"Skip task {task['name']} because of the _")
             continue
 
+        depends_on = []
+        for r in list_dependencies(task):
+            if r not in registers:
+                print(
+                    f"task: {task['name']}\nCannot find key '{r}' in the known variables: {registers.keys()}"
+                )
+                continue
+            depends_on += registers[r]
+
         if "register" in task:
             if task["register"].startswith("_"):
                 print(f"Hiding register {task['register']} because of the _ prefix.")
                 del task["register"]
             else:
-                if "name" in task:
-                    registers[task["register"]] = task
-                print("Task register: %s" % task["register"])
+                registers[task["register"]] = depends_on + [task]
 
         if "set_fact" in task:
             for fact_name in task["set_fact"]:
-                registers[fact_name] = task
-
-        use_registers = []
-
-        if "with_items" in task and "{{" in task["with_items"]:
-            variable_name = task["with_items"].strip(" }{")
-            use_registers.append(variable_name.split(".")[0])
+                registers[fact_name] = depends_on + [task]
 
         module_fqcn = None
         for key in list(task.keys()):
@@ -66,37 +109,11 @@ def extract(tasks, collection_name):
         if not module_fqcn:
             continue
 
-        if task[module_fqcn]:
-            for value in task[module_fqcn].values():
-                if not isinstance(value, str):
-                    continue
-                if "{" not in value:
-                    continue
-                variable_name = value.strip(" }{")
-                use_registers.append(variable_name.split(".")[0])
-
         if module_fqcn not in by_modules:
             by_modules[module_fqcn] = {
                 "blocks": [],
-                "depends_on": [],
-                "use_registers": [],
             }
-        by_modules[module_fqcn]["blocks"] += [task]
-        by_modules[module_fqcn]["use_registers"] += use_registers
-
-    for module_fqcn in by_modules:
-        for r in sorted(list(set(by_modules[module_fqcn]["use_registers"]))):
-            if r in ["item"]:
-                continue
-            if r.startswith("lookup("):
-                continue
-            try:
-                by_modules[module_fqcn]["depends_on"].append(registers[r])
-            except KeyError:
-                print(f"Cannot find definition of '{r}', ensure:")
-                print("  - the variable is properly defined")
-                print("  - the task that define the name has a name")
-                raise
+        by_modules[module_fqcn]["blocks"] += depends_on + [task]
 
     return by_modules
 
@@ -104,12 +121,13 @@ def extract(tasks, collection_name):
 def flatten_module_examples(module_examples):
     result = ""
     blocks = module_examples["blocks"]
-    block_names = [b["name"] for b in blocks]
-    depends_on = [
-        d for d in module_examples["depends_on"] if d["name"] not in block_names
-    ]
-    for block in depends_on + blocks:
-        result += _task_to_string(block) + "\n"
+    seen = []
+
+    for block in blocks:
+        if block in seen:
+            continue
+        seen.append(block)
+        result += "\n" + _task_to_string(block) + "\n"
     return result
 
 
@@ -129,7 +147,7 @@ def inject(target_dir, extracted_examples):
         for l in module_path.read_text().split("\n"):
             if l == 'EXAMPLES = r"""':
                 in_examples_block = True
-                new_content += l + "\n" + examples_section_to_inject
+                new_content += l + "\n" + examples_section_to_inject.lstrip("\n")
             elif in_examples_block and l == '"""':
                 in_examples_block = False
                 new_content += l + "\n"
@@ -137,8 +155,8 @@ def inject(target_dir, extracted_examples):
                 continue
             else:
                 new_content += l + "\n"
+        new_content = new_content.rstrip("\n") + "\n"
         print(f"Updating {module_name}")
-        new_content.rstrip("\n")
         module_path.write_text(new_content)
 
 
@@ -156,9 +174,17 @@ def main():
     galaxy_file = args.target_dir / "galaxy.yml"
     galaxy = yaml.load(galaxy_file.open())
     collection_name = f"{galaxy['namespace']}.{galaxy['name']}"
-    tasks = get_tasks(
-        args.target_dir, "vcenter_vm_scenario1", "prepare_lab", "appliance"
-    )
+    tasks = []
+    for scenario in (
+        "prepare_lab",
+        "vcenter_vm_scenario1",
+        "appliance",
+        "content_library",
+    ):
+        task_dir = (
+            args.target_dir / "tests" / "integration" / "targets" / scenario / "tasks"
+        )
+        tasks += get_tasks(task_dir)
     extracted_examples = extract(tasks, collection_name)
     inject(args.target_dir, extracted_examples)
 
